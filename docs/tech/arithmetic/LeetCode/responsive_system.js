d9877d73681c388f4586f61306bae23475947732
evilirving <dong.yy1916@gmail.com>
2024-03-17 16:23:13 +0800

    feat: 避免无限递归调用

--- docs/tech/arithmetic/LeetCode/responsive_system.js
+++ docs/tech/arithmetic/LeetCode/responsive_system.js
@@ -1,7 +1,8 @@
+
 const rawData = { ok: true, name: 'Cain' }
 // WeakMap 对 key 是弱引用，不影响垃圾回收器的工 作。
 const bucket = new WeakMap() // 桶
-let page
+let page, temp1, temp2
 
 // 桶的结构
 // bucket = {
@@ -13,31 +14,59 @@ let page
 
 const obj = new Proxy(rawData, {
     get(target, key) {
-        console.log('get', key);
+        console.log('get 触发:', key);
         track(target, key)
         return target[key]
     },
 
     set(target, key, newVal) {
-        console.log('set', key);
+        console.log('set 触发:', key);
         target[key] = newVal
         trigger(target, key)
         return true
     }
 })
-
+const effectStack = [] // 副作用函数栈
 let activeEffect  // 存储被注册的副作用函数.
 // 注册副作用函数
 function registerEffect(fn) {
-    activeEffect = fn
-    fn()
+    const effectFn = () => {
+        cleanup(effectFn)
+        activeEffect = effectFn
+        effectStack.push(effectFn)
+        fn()
+        effectStack.pop()
+        activeEffect = effectStack[effectStack.length - 1]
+    }
+    effectFn.source = effectFn.toString()
+    effectFn.deps = []
+    effectFn()
 }
+
+// registerEffect(function fn1() {
+//     console.log('fn1----');
+
+//     registerEffect(function fn2() {
+//         console.log('fn2----');
+//         temp2 = obj.name
+//     })
+
+//     temp1 = obj.ok
+// })
 
 registerEffect(() => {
     console.log('副作用函数执行了');
     page = obj.ok ? obj.name : "not"
 })
 
+function cleanup(effect) {
+    for (let index = 0; index < effect.deps.length; index++) {
+        const deps = effect.deps[index];
+        deps.delete(effect)
+    }
+    // 重置
+    effect.deps.length = 0
+}
 
 function track(target, key) {
     if (!activeEffect) return target[key]
@@ -48,24 +77,37 @@ function track(target, key) {
     }
     let deps = depsMap.get(key)
     if (!deps) {
-        depsMap.set(key, (deps = new Set()))
+        depsMap.set(key, (deps = new Map()))
     }
-    deps.add(activeEffect)
+    // console.log(deps.has(activeEffect),activeEffect.toString(),'----------');
+    !deps.has(activeEffect.source) && deps.set(activeEffect.source, activeEffect)
+    activeEffect.deps.push(deps)
 }
 
 function trigger(target, key) {
     let depsMap = bucket.get(target)
     if (!depsMap) return true
-    let deps = depsMap.get(key)
-    // 确保set陷阱在设置属性值时能够正确地返回一个真值，或者在set陷阱中处理好不存在的属性的情况。
+    const deps = depsMap.get(key)
     if (!deps) return true
-    deps.forEach(fn => fn());
+    // 确保set陷阱在设置属性值时能够正确地返回一个真值，或者在set陷阱中处理好不存在的属性的情况。
+    const depsTorun = new Map(deps)
+    console.log(deps.size, 'deps');
+
+    deps.forEach((key, dep) => {
+        if (dep.source !== activeEffect) depsTorun.set(dep.toString(), dep)
+    })
+    depsTorun.forEach(fn => fn());
 }
 
 
-// obj.name = "Garcia"
 // obj.noExist = '---'
+
+
+// obj.name = "Garcia"
 obj.ok = false
 
 
-console.log(obj,page);
+console.log(obj, page);
+
+
+// TODO  此处存疑,到底 deps 这些东西如何理解,打断点执行的过程,不知道作者到底想表达什么
